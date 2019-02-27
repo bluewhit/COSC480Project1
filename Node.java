b0a8fc8cddf88d9d52d6f858d9244b28145292ac
bluewhit <43395666+bluewhit@users.noreply.github.com>
2019-02-27 14:43:11 -0500

    Add files via upload

--- Node.java
+++ Node.java
@@ -2,45 +2,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 //https://stackoverflow.com/questions/19330731/tree-implementation-in-java-root-parents-and-children
-public class Node<T> {
-    private List<Node<T>> children = new ArrayList<Node<T>>();
-    private Node<T> parent = null;
-    private T data = null;
+public class Node<String> {
+	
+    private List<Node<String>> children = new ArrayList<Node<String>>();
+    private Node<String> parent = null;
+    private String data = null;
 
-    public Node(T data) {
+    public Node(String data) {
         this.data = data;
     }
 
-    public Node(T data, Node<T> parent) {
+    public Node(String data, Node<String> parent) {
         this.data = data;
         this.parent = parent;
     }
 
-    public List<Node<T>> getChildren() {
+    public List<Node<String>> getChildren() {
         return children;
     }
 
-    public void setParent(Node<T> parent) {
+    public void setParent(Node<String> parent) {
         parent.addChild(this);
         this.parent = parent;
     }
 
-    public void addChild(T data) {
-        Node<T> child = new Node<T>(data);
+    public void addChild(String data) {
+        Node<String> child = new Node<String>(data);
         child.setParent(this);
         this.children.add(child);
     }
 
-    public void addChild(Node<T> child) {
+    public void addChild(Node<String> child) {
         child.setParent(this);
         this.children.add(child);
     }
 
-    public T getData() {
+    public String getData() {
         return this.data;
     }
 
-    public void setData(T data) {
+    public void setData(String data) {
         this.data = data;
     }
 
